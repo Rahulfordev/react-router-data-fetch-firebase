9a9c5d48c08002233f9d07b6e406ec84be353803
rahulfordev <rahulforsms@gmail.com>
2023-12-23 14:34:59 +0600

    login add

--- src/pages/Auth/SignIn/SignIn.jsx
+++ src/pages/Auth/SignIn/SignIn.jsx
@@ -1,31 +1,78 @@
 import { NavLink } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "../../../firebase/firebase.config";
+import "./SignIn.css";
+import { toast } from "react-toastify";
 
 const SignIn = () => {
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    if (email === "" && password === "") {
+      return toast.error("Fill up The input fiuld");
+    }
+
+    signInWithEmailAndPassword(email, password);
+    return toast.success("Sign In Successfully!");
+  };
+
   return (
-    <div>
-      <div>SignIn</div>
-      <form action="">
-        <div>
-          <div>
-            <input type="email" placeholder="Enter your email" />
+    <div className="signup-container">
+      <div className="container">
+        <form onSubmit={handleSignIn} className="sign-form" id="sign-form">
+          {/* <div className="sign-form" id="sign-form"> */}
+          <h1 className="htmlForm-title">Sign In</h1>
+          <p className="htmlForm-caption">
+            See your growth and get consulting support!
+          </p>
+          <button className="google-sign-btn">
+            <i className="fa fa-google"></i> Sign up with Google
+          </button>
+
+          <br />
+
+          <div className="google-guide-container">
+            <div className="hr-left"></div>
+            <p className="guide-google">or Sign in with Email</p>
+            <div className="hr-right"></div>
           </div>
-          <div>
-            <input type="password" placeholder="Enter your password" />
-          </div>
-          <div>
-            <button>Login</button>
-          </div>
-        </div>
-      </form>
-      <div>
-        <h5>
-          Do not have an account yet? <NavLink to={"/signup"}>Sign Up</NavLink>
-        </h5>
-      </div>
-      <div>
-        <button>Google</button>
-        <button>Github</button>
-        <button>Facebook</button>
+
+          <label htmlFor="email">
+            Email<span className="star-required">*</span>
+          </label>
+          <input type="email" name="email" placeholder="mail@website.com" />
+
+          <label htmlFor="password">
+            Password<span className="star-required">*</span>
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Min. 8 character"
+          />
+
+          <br />
+          <br />
+          <button className="signup-button" type="submit" id="submit">
+            Sign In
+          </button>
+
+          <p className="have-account-line">
+            Do not have an account yet?{" "}
+            <NavLink to={"/signup"}>Sign up</NavLink>
+          </p>
+
+          <p className="info">
+            &copy; {new Date().getFullYear()} Rahulfordev All rights reserved.
+          </p>
+          {/* </div> */}
+        </form>
       </div>
     </div>
   );
--- src/pages/Auth/SignUp/SignUp.jsx
+++ src/pages/Auth/SignUp/SignUp.jsx
@@ -1,34 +1,105 @@
 import { NavLink } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "../../../firebase/firebase.config";
+import { toast } from "react-toastify";
+
+import "./SignUp.css";
 
 const SignUp = () => {
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  if (error) {
+    return toast.error(error.message);
+  }
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const name = e.target.fullname.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const confirmPass = e.target.cpassword.value;
+    if (email === "" && password === "" && confirmPass === "") {
+      return toast.error("Fill up The input fiuld");
+    }
+
+    if (password !== confirmPass) {
+      return toast.error("Password doesn't match");
+    }
+
+    createUserWithEmailAndPassword(email, password);
+    return toast.success("Sign Up Successfully!");
+  };
+
   return (
-    <div>
-      <div>SignUp</div>
-      <form action="">
-        <div>
-          <div>
-            <input type="name" placeholder="Enter your name" />
+    <div className="signup-container">
+      <div className="container">
+        <form onSubmit={handleSignUp} className="sign-htmlForm" id="sign-form">
+          <h1 className="htmlForm-title">Sign Up</h1>
+          <p className="htmlForm-caption">
+            See your growth and get consulting support!
+          </p>
+          <button className="google-sign-btn">
+            <i className="fa fa-google"></i> Sign up with Google
+          </button>
+
+          <br />
+
+          <div className="google-guide-container">
+            <div className="hr-left"></div>
+            <p className="guide-google">or Sign up with Email</p>
+            <div className="hr-right"></div>
           </div>
-          <div>
-            <input type="email" placeholder="Enter your email" />
-          </div>
-          <div>
-            <input type="password" placeholder="Enter your password" />
-          </div>
-          <div>
-            <button>Sign Up</button>
-          </div>
-        </div>
-      </form>
-      <div>
-        <h5>
-          Already have an account yet? <NavLink to={"/login"}>Sign In</NavLink>
-        </h5>
-      </div>
-      <div>
-        <button>Google</button>
-        <button>Github</button>
-        <button>Facebook</button>
+
+          <label htmlFor="username">
+            Name<span className="star-required">*</span>
+          </label>
+          <input type="text" name="fullname" id="username" placeholder="Name" />
+
+          <label htmlFor="email">
+            Email<span className="star-required">*</span>
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="mail@website.com"
+          />
+
+          <label htmlFor="password">
+            Password<span className="star-required">*</span>
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Min. 8 character"
+          />
+
+          <label htmlFor="cpassword">
+            Confirm Password<span className="star-required">*</span>
+          </label>
+          <input
+            type="password"
+            name="cpassword"
+            id="cpassword"
+            placeholder="Min. 8 character"
+          />
+
+          <br />
+          <br />
+          <button className="signup-button" id="submit">
+            Sign Up
+          </button>
+
+          <p className="have-account-line">
+            Already have an Account? <NavLink to={"/login"}>Sign in</NavLink>
+          </p>
+
+          <p className="info">
+            &copy; {new Date().getFullYear()} Rahulfordev All rights reserved.
+          </p>
+        </form>
       </div>
     </div>
   );
